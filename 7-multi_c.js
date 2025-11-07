4c5f5faec99292018a2d6162fc26813058a64fb0
Cole_Yung <utsukushiotaku17@gmail.com>
2025-11-07 02:04:47 +0200

    Update occurrence validation in multi_c.js
    Refactor input validation to check for positive occurrences.

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,11 @@
 const times = parseInt(process.argv[2]);
 
-if (!times || times < 1) {
-  console.log("Missing number of occurrences");
-} else {
+if (times > 0) {
   let i = 0;
   while (i < times) {
     console.log("C is fun");
     i++;
   }
 }
+// Do nothing if times is missing, invalid, or <= 0
+
